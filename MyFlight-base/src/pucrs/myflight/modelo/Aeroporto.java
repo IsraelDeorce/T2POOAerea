b3d985d06c4c5e8ada92b9fd8de3c5532202ad15
DELL <DELL@DELL-PC.home>
2016-11-01 16:36:19 -0200

    Adicionado atributo pais à classe Aeroporto e método getPais

--- MyFlight-base/src/pucrs/myflight/modelo/Aeroporto.java
+++ MyFlight-base/src/pucrs/myflight/modelo/Aeroporto.java
@@ -4,11 +4,13 @@ public class Aeroporto {
 	private String codigo;
 	private String nome;
 	private Geo loc;
+	private Pais pais;
 	
-	public Aeroporto(String codigo, String nome, Geo loc) {
+	public Aeroporto(String codigo, String nome, Geo loc, Pais pais) {
 		this.codigo = codigo;
 		this.nome = nome;
 		this.loc = loc;
+		this.pais = pais;
 	}
 	
 	public String getCodigo() {
@@ -21,5 +23,9 @@ public class Aeroporto {
 	
 	public Geo getLocal() {
 		return loc;
+	}
+	
+	public Pais getPais(){
+		return pais;
 	}
 }
