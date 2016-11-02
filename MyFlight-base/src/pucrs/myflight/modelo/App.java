cbb3f25edaa6f925960cef1c146b7696140712a4
Israel Deorce <israel.idvj@gmail.com>
2016-11-02 19:43:35 -0200

    Concluída alteração no Gerenciador de Aeronaves para leitura de dados

--- MyFlight-base/src/pucrs/myflight/modelo/App.java
+++ MyFlight-base/src/pucrs/myflight/modelo/App.java
@@ -50,6 +50,8 @@ public class App {
 		GerenciadorAeroportos gerAero = new GerenciadorAeroportos();
 		GerenciadorRotas gerRotas = new GerenciadorRotas();
 		GerenciadorPaises gerPaises = new GerenciadorPaises();
+		GerenciadorAeronaves gerAeron = new GerenciadorAeronaves();
+		
 		try{ 
 			gerPaises.carregaDados();
 		}
@@ -78,5 +80,16 @@ public class App {
 		janela.setGerAeroportos(gerAero);
 		janela.setGerRotas(gerRotas);
 		janela.setVisible(true);
+		
+		
+		try{
+			gerAeron.carregaDados();
+		}catch (IOException e){
+			System.out.println("Impossível ler equipment.dat!");
+			System.out.println("Msg: "+e);
+			System.exit(1);			
 		}
+		
+		
+	}
 }
--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,17 +1,49 @@
 package pucrs.myflight.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
-public class GerenciadorAeronaves {
-
-	private ArrayList<Aeronave> aeronaves;
+public class GerenciadorAeronaves {	
+	private Map<String, Aeronave> aeronaves;
 	
 	public GerenciadorAeronaves() {
-		aeronaves = new ArrayList<>();
+		aeronaves = new TreeMap<String, Aeronave>();
 	}
 	
+	public void carregaDados() throws IOException {
+		Path path = Paths.get("equipment.dat");
+		try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+			String linha = br.readLine();
+			System.out.println("Cabeçalho: " + linha);
+			while ((linha = br.readLine()) != null) {
+				Scanner scan = new Scanner(linha).useDelimiter(";");
+				String codigo, descricao;
+				int capacidade = 0; //Observe que a inicialização com 0 pode causar erros.
+				codigo = scan.next();
+				descricao = scan.next();
+				try{
+					capacidade = Integer.parseInt(scan.next());
+										
+				}catch (NumberFormatException e) {
+				    e.printStackTrace();
+				}							
+				Aeronave aux = new Aeronave(codigo, descricao, capacidade);
+				aeronaves.put(aux.getCodigo(), aux);			
+			}					
+		}
+		System.out.println("Total Aeronaves: " + aeronaves.size());
+	}
+	/*
 	public void adicionar(Aeronave av) {
 		aeronaves.add(av);	
 	}
@@ -45,4 +77,5 @@ public class GerenciadorAeronaves {
 		}
 		return null; // nÃ£o achamos!
 	}
+	*/
 }
