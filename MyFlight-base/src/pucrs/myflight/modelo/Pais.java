4c80639f00737316a9111dd52aa6c1f01bd5b7ed
DELL <DELL@DELL-PC.home>
2016-11-02 15:22:34 -0200

    Corrigindo ato ributo codigde Pais. Alterado tipo de int para String

--- MyFlight-base/src/pucrs/myflight/modelo/Pais.java
+++ MyFlight-base/src/pucrs/myflight/modelo/Pais.java
@@ -1,19 +1,19 @@
 package pucrs.myflight.modelo;
 
 public class Pais {
+	private String codigo;
 	private String nome;
-	private int codigo;
 	
-	public Pais(String nome, int codigo){
-		this.nome = nome;
+	public Pais(String codigo, String nome){
 		this.codigo = codigo;
+		this.nome = nome;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-	public int getCodigo() {
+	public String getCodigo() {
 		return codigo;
 	}
 	
